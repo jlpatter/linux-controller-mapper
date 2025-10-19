f66901519e1a989feb907faf7e8f0099731d5be3
Joshua Patterson <joshua.patterson745@gmail.com>
2025-10-18 22:25:41 -0500

    Fixed mouse movement

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use std::time::SystemTime;
 use enigo::{Coordinate, Enigo, Mouse, Settings};
 use gilrs::{Axis, Event, Gilrs};
 use gilrs::EventType::AxisChanged;
+
+
+const DEADZONE: f32 = 0.05;
+const MOUSE_SPEED_MODIFIER: f32 = 0.5;
 
 fn main() {
     let mut gilrs = Gilrs::new().unwrap();
@@ -11,8 +16,11 @@ fn main() {
         println!("{} is {:?}", gamepad.name(), gamepad.power_info());
     }
 
-    let mut mouse_x_amt = 0;
-    let mut mouse_y_amt = 0;
+    let (mouse_x_pix, mouse_y_pix) = enigo.location().unwrap();
+    let mut mouse_x_pos = mouse_x_pix as f32;
+    let mut mouse_y_pos = mouse_y_pix as f32;
+    let mut mouse_x_amt = 0.0;
+    let mut mouse_y_amt = 0.0;
 
     loop {
         // Examine new events
@@ -21,16 +29,21 @@ fn main() {
 
             if let AxisChanged(axis, amt, _) = event {
                 if axis == Axis::LeftStickX {
-                    mouse_x_amt = (amt * 2.0) as i32;
-
+                    mouse_x_amt = if amt.abs() > DEADZONE {amt * MOUSE_SPEED_MODIFIER} else {0.0};
                 } else if axis == Axis::LeftStickY {
-                    mouse_y_amt = -(amt * 2.0) as i32
-
+                    mouse_y_amt = if amt.abs() > DEADZONE {-amt * MOUSE_SPEED_MODIFIER} else {0.0};
                 }
             }
         }
 
-        enigo.move_mouse(mouse_x_amt, 0, Coordinate::Rel).unwrap();
-        enigo.move_mouse(0, mouse_y_amt, Coordinate::Rel).unwrap();
+        if mouse_x_amt.abs() > 0.0 || mouse_y_amt.abs() > 0.0 {
+            mouse_x_pos += mouse_x_amt;
+            mouse_y_pos += mouse_y_amt;
+            enigo.move_mouse(mouse_x_pos as i32, mouse_y_pos as i32, Coordinate::Abs).unwrap();
+        } else {
+            let (mouse_x_pix, mouse_y_pix) = enigo.location().unwrap();
+            mouse_x_pos = mouse_x_pix as f32;
+            mouse_y_pos = mouse_y_pix as f32;
+        }
     }
 }
