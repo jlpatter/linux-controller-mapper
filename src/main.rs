ad44ad43fe52412a8389fd50db5082b49d5b2e53
Joshua Patterson <joshua.patterson745@gmail.com>
2025-11-27 17:07:38 -0600

    Switch unwrap to unwrap_or

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::time::SystemTime;
 use enigo::{Coordinate, Enigo, Mouse, Settings};
 use gilrs::{Axis, Event, Gilrs};
 use gilrs::EventType::AxisChanged;
@@ -16,7 +15,7 @@ fn main() {
         println!("{} is {:?}", gamepad.name(), gamepad.power_info());
     }
 
-    let (mouse_x_pix, mouse_y_pix) = enigo.location().unwrap();
+    let (mouse_x_pix, mouse_y_pix) = enigo.location().unwrap_or((0, 0));
     let mut mouse_x_pos = mouse_x_pix as f32;
     let mut mouse_y_pos = mouse_y_pix as f32;
     let mut mouse_x_amt = 0.0;
@@ -41,7 +40,7 @@ fn main() {
             mouse_y_pos += mouse_y_amt;
             enigo.move_mouse(mouse_x_pos as i32, mouse_y_pos as i32, Coordinate::Abs).unwrap();
         } else {
-            let (mouse_x_pix, mouse_y_pix) = enigo.location().unwrap();
+            let (mouse_x_pix, mouse_y_pix) = enigo.location().unwrap_or((mouse_x_pos as i32, mouse_y_pos as i32));
             mouse_x_pos = mouse_x_pix as f32;
             mouse_y_pos = mouse_y_pix as f32;
         }
