9d0991793f3011123fc76f261704a009792b3532
Joshua Patterson <joshua.patterson745@gmail.com>
2025-11-28 12:27:26 -0600

    WIP - Started adding basic GUI layout

--- src/ui/main_frontend.rs
+++ src/ui/main_frontend.rs
@@ -1,13 +1,12 @@
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
-use iced::Task;
-use iced::widget::{button, row, text, Row};
+use iced::alignment::Horizontal;
+use iced::{Length, Task};
+use iced::widget::{button, column, row, text, Column, Container};
 use crate::backend::controller_handler::handle_controller_input;
 
 #[derive(Clone, Debug)]
 pub enum Message {
-    Increment,
-    Decrement,
     Activate,
     Activated(()),
     Deactivate,
@@ -15,21 +14,12 @@ pub enum Message {
 
 #[derive(Default)]
 pub struct Mapper {
-    value: i64,
     is_handler_running: Arc<AtomicBool>
 }
 
 impl Mapper {
     pub fn update(&mut self, message: Message) -> Task<Message> {
         match message {
-            Message::Increment => {
-                self.value += 1;
-                Task::none()
-            }
-            Message::Decrement => {
-                self.value -= 1;
-                Task::none()
-            }
             Message::Activate => Task::perform(
                 {
                     self.is_handler_running.store(true, Ordering::Relaxed);
@@ -45,19 +35,29 @@ impl Mapper {
         }
     }
 
-    pub fn view(&self) -> Row<Message> {
-        // The buttons
-        let increment = button("+").on_press(Message::Increment);
-        let decrement = button("-").on_press(Message::Decrement);
-
-        // The number
-        let counter = text(self.value);
-
+    pub fn view(&self) -> Column<Message> {
         let activate = button("Activate").on_press(Message::Activate);
         let deactivate = button("Deactivate").on_press(Message::Deactivate);
 
-        // The layout
-        row![decrement, counter, increment, activate, deactivate]
-
+        column![
+            row![
+                column![
+                    text("D-Pad"),
+                    Container::new(button("Up")).width(Length::Fill).align_x(Horizontal::Center),
+                    row![
+                        Container::new(button("Left")).width(Length::Fill),
+                        Container::new(button("Right")).width(Length::Fill).align_x(Horizontal::Right)
+                    ],
+                    Container::new(button("Down")).width(Length::Fill).align_x(Horizontal::Center)
+                ].width(Length::FillPortion(1)).spacing(25),
+                column![].width(Length::FillPortion(1)),
+                column![].width(Length::FillPortion(1)),
+            ].width(Length::Fill).height(Length::Fill),
+            row![].width(Length::Fill).height(Length::Fill),
+            row![
+                Container::new(activate).width(Length::Fill).align_x(Horizontal::Right),
+                Container::new(deactivate).align_x(Horizontal::Right),
+            ].spacing(10).width(Length::Fill).height(Length::Fill)
+        ].spacing(10)
     }
 }
