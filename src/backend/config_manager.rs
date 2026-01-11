933d43fa280a4d87683c6471db8b6c090cf77f24
Joshua Patterson <joshua.patterson745@gmail.com>
2026-01-11 11:24:36 -0600

    Fix serialization

--- src/backend/config_manager.rs
+++ src/backend/config_manager.rs
@@ -4,11 +4,10 @@ use gilrs::{Axis, Button};
 use uuid::Uuid;
 use serde::{Deserialize, Serialize};
 
-// TODO: Fix the members so they can be serialized and deserialized!
 #[derive(Serialize, Deserialize)]
 struct LayoutConfig {
     controller_name: String,
     controller_uuid: Uuid,
     button_map: HashMap<Button, Key>,
-    axis_map: HashMap<Axis, String>,  // TODO: Figure out a better type for this!
+    axis_map: HashMap<Axis, String>,
 }
