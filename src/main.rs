c6eff4c50bef685908904295862d768253db3702
Joshua Patterson <joshua.patterson745@gmail.com>
2026-01-11 11:00:47 -0600

    Fix lint

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,5 @@ mod backend;
 use crate::ui::main_frontend::Mapper;
 
 fn main() {
-    // iced::run("Linux Controller Mapper", Mapper::update, Mapper::view).unwrap();
     iced::daemon("Linux Controller Mapper", Mapper::update, Mapper::view).subscription(Mapper::subscription).run_with(Mapper::new).unwrap();
 }
